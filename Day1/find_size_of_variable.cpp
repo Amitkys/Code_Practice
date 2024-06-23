afbf7e1b96cbfa847c6fc20119c681b80b990209
Amitkys <amitkys59@gmail.com>
2024-06-23 11:51:13 +0530

    second problem of day 2

---
+++ Day1/find_size_of_variable.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int integer;
+    float floatt;
+    char character;
+    double doublee;
+
+
+    cout << "size of int: " << sizeof(integer) << endl;
+    cout << "size of float: " << sizeof(floatt) << endl;
+    cout << "size of char: " << sizeof(character) << endl;
+    cout << "size of double: " << sizeof(double) << endl;
+
+
+
+    return 0;
+}
